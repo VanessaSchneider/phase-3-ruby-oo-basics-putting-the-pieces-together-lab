f2c753b3c662071dd960aa5787553601154104df
Vanessa Schneider <vanessajwade@gmail.com>
2022-02-24 00:44:14 -0500

    commit tree b1bab060ef53fdea068a22b3355c21d32659931b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
 
 
+class Book
+    attr_accessor: (title = "And Then There Were None", author name, page count, genre)
+    
+
+    def turn_page
+    
+
+
+
+
+        puts "Flipping the page...wow, you read fast!"
+
+
+end
+
+
+
+
+
+class Dog
+    attr_accessor :name, :breed
+  
+    def initialize(name, breed = "Mutt")
+      @name = name
+      @breed = breed
+    end
+  
+  end
